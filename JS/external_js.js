f0c881987809c6b19c0e79ece5b334e6d469ca63
PapoteDetres <desertcoqui@yahoo.com>
2016-11-03 17:01:19 -0500

    If else examples, images,JS files

---
+++ JS/external_js.js
@@ -0,0 +1,12 @@
+/**
+ * Created by PapoteDetres1 on 11/3/16.
+ */
+'use strict'
+var success= true;
+var message;
+
+if (success) {
+    message = "Operation was successful.";
+} else {
+    message = "Oops, something went wrong.";
+}
---
+++ JS/if-else.js
@@ -0,0 +1,13 @@
+/**
+ * Created by PapoteDetres1 on 11/3/16.
+ */
+'use strict'
+
+var grade= (70+80+95)/3;
+if (grade>=80){
+    console.log("good job!");
+}   else {
+    console.log("you suck!");
+}
+
+
