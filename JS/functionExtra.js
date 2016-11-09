2a3ed0c811159a1f9547611580aa2dcd7713099a
PapoteDetres <desertcoqui@yahoo.com>
2016-11-09 09:02:37 -0600

    I

---
+++ JS/functionExtra.js
@@ -0,0 +1,50 @@
+/**
+ * Created by PapoteDetres1 on 11/8/16.
+ */
+'use strict'
+
+//create four functions
+//
+//that take two numbers as input and produce an output that is the result of the respective operation
+//
+//add
+//subtract
+//divide
+//multiply
+//test these with console logs
+//
+//hook these functions up to user input
+//
+//for each of the above functions...
+//
+//    make sure the user input is valid
+//prompt the user for the two numbers
+//perform the calculation using your functions
+//allow the user to choose which operation they want to perform, then prompt for inputs
+//
+//                                                                               ** Bonus **
+//
+//                                                                               define your functions in terms of each other
+//
+//do not use the * operator inside of your multiply function
+//do not subtract the two numbers inside of your subtract function
+//instead call your add function
+
+var a = parseFloat (prompt("enter value a"));
+var b = parseFloat (prompt("enter value b"));
+
+
+function plusTwo(a,b) {
+    return a+b;
+}
+
+
+
+
+var returnValue = plusTwo(a,b);
+
+//var tenPlusTwo = plusTwo(10, 2);
+//var fiveAndFive = plusTwo(60, 5);
+
+console.log(returnValue);
+alert("Your answer is " + returnValue);
--- JS/user-interaction.js
+++ JS/user-interaction.js
@@ -1,7 +1,7 @@
 /**
  * Created by PapoteDetres1 on 11/7/16.
  */
-"use strict";
+"use strict"
 
 // TODO: Ask the user for their name.
 //       Keep asking if an empty input is provided.
@@ -11,4 +11,11 @@
 // TODO: Ask the user if they like pizza.
 //       Based on their answer show a creative alert message.
 
-var name= prompt("What is your name?");
+//var name= prompt("What is your name?");
+
+//function add (x) {
+//    var result= x+1;
+//    return result;
+//}
+//
+
